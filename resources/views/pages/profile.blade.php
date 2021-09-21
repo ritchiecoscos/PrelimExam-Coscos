ae4110352f8d4fa3b79597cf1a273926ffd031fc
ritchie <johnritchiecoscos52@gmail.com>
2021-09-21 21:34:21 +0800

    First commit

---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coscos Motorshop | Profile</title>
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>                 
+<body>
+    <h1>Profile Page</h1>
+    <a  href="{{url('/')}}">Home</a></li>
+    <a  href="{{url('/profile')}}"> Profile Page</a></li>
+    <a  href="{{url('/ceo')}}">CEO</a></li>
+     
+</body>
+</html>
